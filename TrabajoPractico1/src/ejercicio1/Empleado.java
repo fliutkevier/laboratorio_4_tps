43e94cec89dbc00a5b034fbb8faf0565238cd96f
Franco Liutkevier <79414328+fliutkevier@users.noreply.github.com>
2025-03-24 12:05:07 -0300

    Add files via upload

---
+++ TrabajoPractico1/src/ejercicio1/Empleado.java
@@ -0,0 +1,26 @@
+package ejercicio1;
+
+public class Empleado {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	private int id;
+	private String nombre;
+	private int edad;
+}
