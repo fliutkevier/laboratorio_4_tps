e1683c142ad7489567b1efa45e3bf0d881a273f8
Belu Verchy <mbvercell@hotmail.com>
2025-03-24 23:03:17 +0100

    Add files via upload
    Se creó la clase Principal y la creación de un empleado mostrando su información correspondiente

---
+++ TrabajoPractico1/src/ejercicio1/Principal.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Empleado empleado1 = new Empleado("Sara", 27);
+		
+		System.out.println(empleado1.toString());
+		
+	}
+
+}
