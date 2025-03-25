db602e14532054e17ae9507ca4470a10948eeab6
Abril <lopez.abrilagustina6@gmail.com>
2025-03-25 14:28:07 -0300

    empleado 2, llamada metodo proximo id

--- TrabajoPractico1/src/ejercicio1/Principal.java
+++ TrabajoPractico1/src/ejercicio1/Principal.java
@@ -5,8 +5,11 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Empleado empleado1 = new Empleado("Sara", 27);
+		Empleado empleado2 = new Empleado();
 		
 		System.out.println(empleado1.toString());
+		
+		System.out.println( Empleado.devuelveProximoID() + ".");
 		
 	}
 
