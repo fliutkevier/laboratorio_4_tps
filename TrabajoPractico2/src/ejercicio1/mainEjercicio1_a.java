bc564f3f20f6875cf2cba74406978704e91f2349
Abril <lopez.abrilagustina6@gmail.com>
2025-04-03 21:26:37 -0300

    Clase mainEjercicio1_a, Array con 5 profesores

---
+++ TrabajoPractico2/src/ejercicio1/mainEjercicio1_a.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+import java.util.ArrayList; 
+public class mainEjercicio1_a {
+	
+	public static void main(String[] args) {
+		ArrayList<Profesor> ListProf = new ArrayList<Profesor>(5);
+		ListProf.add(new Profesor());
+		ListProf.add(new Profesor());
+		ListProf.add(new Profesor());
+		ListProf.add(new Profesor());
+		ListProf.add(new Profesor());
+		
+	}
+
+}
