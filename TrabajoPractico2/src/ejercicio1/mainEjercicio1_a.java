c839e780dd8cf76a8bd6a976d914d07ab639b728
VickyLuques <luquesvictoria559@gmail.com>
2025-04-05 22:38:24 -0300

    Se cargaron los profesores y se agrego el ListIterator (ejercicio 1a)

--- TrabajoPractico2/src/ejercicio1/mainEjercicio1_a.java
+++ TrabajoPractico2/src/ejercicio1/mainEjercicio1_a.java
@@ -1,14 +1,23 @@
 package ejercicio1;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.ListIterator;
 public class mainEjercicio1_a {
 	
 	public static void main(String[] args) {
 		ArrayList<Profesor> ListProf = new ArrayList<Profesor>(5);
-		ListProf.add(new Profesor());
-		ListProf.add(new Profesor());
-		ListProf.add(new Profesor());
-		ListProf.add(new Profesor());
-		ListProf.add(new Profesor());
+		ListProf.add(new Profesor("Marcos", 42, "Secundario", 15));
+		ListProf.add(new Profesor("Silvina", 30, "Primario", 6));
+		ListProf.add(new Profesor("Florencia", 35, "Secundario", 12));
+		ListProf.add(new Profesor("Nicolas", 33, "Primario", 10));
+		ListProf.add(new Profesor("Emilse", 38, "Secundario", 13));
+		
+		System.out.println("Listado de profesores \n");
+		ListIterator<Profesor> it = ListProf.listIterator();
+		while(it.hasNext())
+		{
+			Profesor p = (Profesor) it.next();
+			System.out.println(p.toString());
+		}
 		
 	}
 
