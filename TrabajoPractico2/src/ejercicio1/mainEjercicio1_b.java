2505cb9f6b693e05e370db635807429231c2d02b
Belu Verchy <mbvercell@hotmail.com>
2025-04-06 17:22:57 +0200

    Carga de profesores en TreeSet

--- TrabajoPractico2/src/ejercicio1/mainEjercicio1_b.java
+++ TrabajoPractico2/src/ejercicio1/mainEjercicio1_b.java
@@ -8,11 +8,11 @@ public class mainEjercicio1_b {
 	public static void main(String[] args) {
 		
 		TreeSet<Profesor> listProf = new TreeSet<Profesor>();
-		listProf.add(new Profesor());
-		listProf.add(new Profesor());
-		listProf.add(new Profesor());
-		listProf.add(new Profesor());
-		listProf.add(new Profesor());
+		listProf.add(new Profesor("Lucas", 45, "Secundario", 18));
+		listProf.add(new Profesor("Sabrina", 43, "Primario", 19));
+		listProf.add(new Profesor("Lara", 35, "Primario", 10));
+		listProf.add(new Profesor("Fabricio", 27, "Secundario", 3));
+		listProf.add(new Profesor("Carla", 39, "Secundario", 13));
 		
 		Iterator<Profesor>it = listProf.iterator();
 		while (it.hasNext()) {
