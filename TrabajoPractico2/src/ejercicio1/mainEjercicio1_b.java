c9746a37b85197a381885249069e3ebee5fb5b2d
VickyLuques <luquesvictoria559@gmail.com>
2025-04-05 22:42:58 -0300

    Se agrego la clase mainEjercicio1b y un TreeSet (ejercicio 1b)

---
+++ TrabajoPractico2/src/ejercicio1/mainEjercicio1_b.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+
+import java.util.TreeSet;
+
+public class mainEjercicio1_b {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Profesor> listProf = new TreeSet<Profesor>();
+		listProf.add(new Profesor());
+		listProf.add(new Profesor());
+		listProf.add(new Profesor());
+		listProf.add(new Profesor());
+		listProf.add(new Profesor());
+	}
+
+}
