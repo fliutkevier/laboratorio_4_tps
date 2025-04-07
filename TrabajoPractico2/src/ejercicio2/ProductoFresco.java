71e16c2e710b3c35f57530f1e10339c1bb0d69f3
Belu Verchy <mbvercell@hotmail.com>
2025-04-07 17:25:05 +0200

    Comentarios de cada parte de la clase
    Para mejor y rápida visualización

--- TrabajoPractico2/src/ejercicio2/ProductoFresco.java
+++ TrabajoPractico2/src/ejercicio2/ProductoFresco.java
@@ -1,15 +1,19 @@
 package ejercicio2;
 
 public class ProductoFresco extends Producto{
+
+	// Atributos ------------------
 	private String Fecha_Envasado;
 	private String Pais_Origen;
 
+	// Constructores ------------------
 	public ProductoFresco(String Fecha, int Nro_lote, String Fecha_Envasado, String Pais_Origen) {
 		super(Fecha, Nro_lote);
 		this.Pais_Origen = Pais_Origen;
 		this.Fecha_Envasado = Fecha_Envasado;
 	}
 
+	// Getters y setters ------------------
 	public String getFecha_Envasado() {
 		return Fecha_Envasado;
 	}
