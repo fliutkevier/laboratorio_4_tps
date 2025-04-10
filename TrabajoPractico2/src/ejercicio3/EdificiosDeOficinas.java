cdc4f02fc5935c8bd14700155c6b43d2dfd6a43f
Franco Liutkevier <79414328+fliutkevier@users.noreply.github.com>
2025-04-10 13:50:14 -0300

    Constructores en Edificio de oficinas, Interfaz implementada

--- TrabajoPractico2/src/ejercicio3/EdificiosDeOficinas.java
+++ TrabajoPractico2/src/ejercicio3/EdificiosDeOficinas.java
@@ -2,8 +2,21 @@ package ejercicio3;
 
 public class EdificiosDeOficinas implements IEdificio {
 	private int NumeroOficinas;
+	private double Superficie;
 	
 	//constructores
+	
+	public EdificiosDeOficinas()
+	{
+		NumeroOficinas = 0;
+		Superficie = 0;
+	}
+	
+	public EdificiosDeOficinas(int numeroOficinas, double superficie)
+	{
+		this.NumeroOficinas = numeroOficinas;
+		this.Superficie = superficie;
+	}
 
 	public int getNumeroOficinas() {
 		return NumeroOficinas;
@@ -13,6 +26,9 @@ public class EdificiosDeOficinas implements IEdificio {
 		NumeroOficinas = numeroOficinas;
 	}
 	
-	
+	public double getSuperficieEdificio()
+	{
+		return Superficie;
+	}
 
 }
