d9a456d77d34259eae018967a30ed76f9ea83936
Franco Liutkevier <79414328+fliutkevier@users.noreply.github.com>
2025-04-15 17:45:37 -0300

    Nueva clase Persona y metodo exception

---
+++ TrabajoPractico3/src/ejercicio1/Persona.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class Persona {
+	private String _nombre;
+	private String _Apellido;
+	private String _dni;
+	
+	public static boolean verificarDniInvalido() throws DniInvalido
+	{
+		//retorna verdadero en caso de que no contenga letras, sino hace throw.
+	}
+}
