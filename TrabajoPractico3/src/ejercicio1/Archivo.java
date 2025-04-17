ef48cc25e73506c66b5a96582f3c346f1bc819ac
Belu Verchy <mbvercell@hotmail.com>
2025-04-17 17:18:36 +0200

    Creación clase Archivo
    Con función para leer un archivo en lista (sin desarrollar)

---
+++ TrabajoPractico3/src/ejercicio1/Archivo.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Archivo {
+	
+	private String ruta = "Personas.txt";
+	
+	public List<String> lecturaPersonas() {
+		
+		List<String> lista = new ArrayList<>();
+		return lista;
+	}
+}
