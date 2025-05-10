1e62fddb5ee35927dae00569b733f6284f5fec1b
VickyLuques <luquesvictoria559@gmail.com>
2025-05-09 22:04:26 -0300

    lblNombre y txtNombre (PanelAgregarPelicula)

--- TrabajoPractico5/src/TP5/PanelAgregarPelicula.java
+++ TrabajoPractico5/src/TP5/PanelAgregarPelicula.java
@@ -5,22 +5,42 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
 
 public class PanelAgregarPelicula extends JPanel {
 	public PanelAgregarPelicula() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblId = new JLabel("ID");
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
-		gbc_lblId.gridx = 4;
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.gridx = 3;
 		gbc_lblId.gridy = 1;
 		add(lblId, gbc_lblId);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
+		gbc_lblNombre.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNombre.gridx = 3;
+		gbc_lblNombre.gridy = 3;
+		add(lblNombre, gbc_lblNombre);
+		
+		txtNombre = new JTextField();
+		GridBagConstraints gbc_txtNombre = new GridBagConstraints();
+		gbc_txtNombre.insets = new Insets(0, 0, 0, 5);
+		gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNombre.gridx = 5;
+		gbc_txtNombre.gridy = 3;
+		add(txtNombre, gbc_txtNombre);
+		txtNombre.setColumns(10);
 	}
 
 	private static final long serialVersionUID = 1L;
+	private JTextField txtNombre;
 }
