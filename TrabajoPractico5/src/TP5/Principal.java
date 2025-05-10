4c3b3ad9dbfe72810b5b82b03902ae42def0ba61
VickyLuques <luquesvictoria559@gmail.com>
2025-05-09 22:00:28 -0300

    main en clase Principal

--- TrabajoPractico5/src/TP5/Principal.java
+++ TrabajoPractico5/src/TP5/Principal.java
@@ -1,5 +1,20 @@
 package TP5;
 
-public class Principal {
+import java.awt.EventQueue;
 
+public class Principal {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrograma frame = new VentanaPrograma();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
 }
--- TrabajoPractico5/src/TP5/VentanaPrograma.java
+++ TrabajoPractico5/src/TP5/VentanaPrograma.java
@@ -15,25 +15,6 @@ public class VentanaPrograma extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPrograma frame = new VentanaPrograma();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public VentanaPrograma() {
 		setTitle("Programa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
